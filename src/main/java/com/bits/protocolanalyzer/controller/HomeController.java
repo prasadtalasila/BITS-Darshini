4fa0aa676df414db1ed188464e53b0914dbd97f0
amit <amit@webexports.co.in>
2015-10-03 01:27:27 +0530

    adds spring configuration files and pom.xml file.

---
+++ src/main/java/com/bits/protocolanalyzer/controller/HomeController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bits.protocolanalyzer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author amit
+ */
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+	@RequestMapping
+	public String home() {
+		return "home";
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : home
+    Created on : 30 Sep, 2015, 1:46:00 AM
+    Author     : amit
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Spring Loaded! Dispatcher Servlet working!</h1>
+    </body>
+</html>
