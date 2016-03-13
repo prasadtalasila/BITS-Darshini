4cc56a7d264f5a52d30c18ada3bc5007c25c3952
sukantoguha <sukantoguha@gmail.com>
2016-03-14 00:25:50 +0530

    Adding signup page view

--- src/main/webapp/WEB-INF/js/views/login-view.js
+++ src/main/webapp/WEB-INF/js/views/login-view.js
@@ -1,19 +1,20 @@
 window.LoginView = Backbone.View.extend({
-		el : $('.container'),
-      	initialize: function () {
-      	},
-      	events: {
-			'click #signinButton' : 'login',
-			'click #signupButton' : 'signup'
-		},
+        el : $('.container'),
+        initialize: function () {
+        },
+        events: {
+            'click #signinButton' : 'login',
+            'click #signupRedirectButton' : 'signupRedirect',
+            'click #signupButton' : 'signup'
+        },
 
-		login : function(event) {
-			event.preventDefault();
-        	var formValues = {
-            	email: $('#inputEmail').val(),
-            	password: $('#inputPassword').val()
-        	};
-        	 $.ajax({
+        login : function(event) {
+            event.preventDefault();
+            var formValues = {
+                email: $('#inputEmail').val(),
+                password: $('#inputPassword').val()
+            };
+             $.ajax({
             url:'/protocolanalyzer/signin',
              type:'POST',
              contentType: 'application/json; charset=utf-8',
@@ -24,42 +25,48 @@ window.LoginView = Backbone.View.extend({
                     app.navigate("#/home");
                  }
                  else if(data ==="failure"){ 
-                     alert("Error logging in, please check your details");
+                    alert("Error logging in, please check your details");
                  }
              },
              error:function(){
                 console.log("Error logging in");
              }
-        	 });			
-    	},
-    	signup : function(event){
-    		var formValues = {
-            	email: $('#inputEmail').val(),
-            	password: $('#inputPassword').val()
-        	};
-        	$.ajax({
+             });            
+        },
+        signupRedirect : function(){
+            $('#popup1').w2popup();
+        },
+        signup : function(){
+            var registerValues = {
+              email:$('#w2ui-popup #registerEmail').val(),
+              password: $('#w2ui-popup #registerPassword').val()
+          };
+        $.ajax({
             url:'/protocolanalyzer/signup',
-             type:'POST',
-             contentType: 'application/json; charset=utf-8',
-             dataType:'text',
-             data: JSON.stringify(formValues),
-             success:function (data) {
-                 if(data === "success") {
-                 	alert("User successfully registered.");
-                    app.navigate("#");
-                 }
-                 else if(data === "failure"){ 
-                     alert("An account with this email ID already exists");
-                 }
-             },
-             error:function(){
+            type:'POST',
+            contentType: 'application/json; charset=utf-8',
+            dataType:'text',
+            data: JSON.stringify(registerValues),
+              success:function (data) {
+                  if(data === "success") {
+                    alert("User successfully registered.");
+                    w2popup.close();
+                     app.navigate("#");
+                  }
+                  else if(data === "failure"){ 
+                    w2popup.close();
+                    alert("An account with this email ID already exists");
+                  }
+              },
+              error:function(){
+                w2popup.close();
                 alert("There was an issue connecting at this time, please try again later");
-             }
-        	 });
-    	},
-		render: function () {
-			$(this.el).html(this.template());
-        	return this;
-		}
-	});
+              }
+           });
+        },
+        render: function () {
+            $(this.el).html(this.template());
+            return this;
+        }
+    });
 
