c6b531ee095ed26bafb33a15cdd621a2e7de13c6
sukantoguha <sukantoguha@gmail.com>
2016-04-29 12:19:16 +0530

    Graph validation algorithm implemented

--- src/main/webapp/WEB-INF/js/views/analysis-view.js
+++ src/main/webapp/WEB-INF/js/views/analysis-view.js
@@ -10,18 +10,41 @@ window.AnalysisView = Backbone.View.extend({
 			this.delegateEvents();
 		},
 		populateTable :function(){
+      var sessionName = sessionStorage.getItem('sessionName');
+      var layers = sessionStorage.getItem('layers').split(',');
+
+      var layerCount =0; //for knowing number of ajax requests
+      var lowerHalfAppend = document.getElementById('lowerHalf');
+      for(var i =1;i<layers.length -1;i++){
+        layerCount++;
+        var newDivId = 'dataContainer'+i;
+        var newDiv = document.createElement('section');
+        newDiv.setAttribute('id',newDivId);
+        lowerHalfAppend.appendChild(newDiv);
+        newDiv.outerHTML = '<section>'+
+          '<article>'+
+            '<details>'+
+              '<summary >'+layers[i].charAt(0).toUpperCase() + layers[i].slice(1)+'</summary>'+
+              '<details>'+
+                '<summary id = dataContainer'+ i+' ></summary>'+
+              '</details>'+
+            '</details>'+
+          '</article>'+
+          '</section>';
+      }
+      
 			$.ajax({
-			url : 'http://localhost:9200/protocol_session_1011452306/ethernet/_search',
+			url : 'http://localhost:9200/protocol_session_-1001725913/ethernet/_search?&size=214',
 			type : 'GET',
 			contentType : 'application/json; charset=utf-8',
 			dataType : 'text',
 			success : function(data) {
 				alert("Success connecting to elasticsearch..");
-				globalData = JSON.parse(data);
-				rows = globalData["hits"]["hits"];
-  			var td, tr;
-  			$("#packetInfo tbody tr").remove(); // clean table
-  			var tdata = $("#packetInfo tbody")
+        var globalData = JSON.parse(data);
+        rows = globalData["hits"]["hits"];
+        var td, tr;
+        $("#packetInfo tbody tr").remove(); // clean table
+        var tdata = $("#packetInfo tbody")
   			for(var row in rows){
     			var rowSource = rows[row]["_source"]
     			tr = $("<tr>");
@@ -53,18 +76,18 @@ window.AnalysisView = Backbone.View.extend({
     			for(var key in botInfo){
       				stringToWrite+= key + ": " + JSON.stringify(botInfo[key]) + "\n";
     			}
-    			$("#dataContainer").show();
-    			$("#dataContainer").text(stringToWrite);
+    			//$("#dataContainer1").show();
+    			$("#dataContainer1").text(stringToWrite);
   			});
   			//mouseover should change the cursor back to s-resize
   			$("#lowerHalf").mouseover( function() {
-  				if($("#dataContainer").is(":visible")) {
+  				if($("#dataContainer1").is(":visible")) {
   					$("#lowerHalf").css('cursor', 's-resize');
   				}
   			});
   			//close data container
   			$("#lowerHalf").click( function() {
-  				$("#dataContainer").hide();
+  				//$("#dataContainer1").hide();
   				$("#lowerHalf").css('cursor', 'default');
   			});
 			},
@@ -72,8 +95,47 @@ window.AnalysisView = Backbone.View.extend({
 				alert("Error connecting to elasticsearch!!")
 			}
 		});
+    for(var counter = 2;counter<=layerCount;counter++){
+    (function(counter)
+    {
+    $.ajax({
+      url : 'http://localhost:9200/protocol_session_-1001725913/'+layers[counter]+'/_search?&size=214',
+      type : 'GET',
+      contentType : 'application/json; charset=utf-8',
+      dataType : 'text',
+      success : function(data) {
+        var globalData = JSON.parse(data);
+        //update handlers
+        $("#packetInfo tbody tr").click( function(){
+          var ind = $(this).index();
+          botInfo = globalData["hits"]["hits"][ind];
+          stringToWrite =  "";
+          for(var key in botInfo){
+              stringToWrite+= key + ": " + JSON.stringify(botInfo[key]) + "\n";
+          }
+          //$("#dataContainer2").show();
+          $("#dataContainer"+counter).text(stringToWrite);
+        });
+        //mouseover should change the cursor back to s-resize
+        $("#lowerHalf").mouseover( function() {
+          if($("#dataContainer"+counter).is(":visible")) {
+            $("#lowerHalf").css('cursor', 's-resize');
+          }
+        });
+        //close data container
+        $("#lowerHalf").click( function() {
+          //$("#dataContainer2").hide();
+          $("#lowerHalf").css('cursor', 'default');
+        });
+      },
+      error : function() {
+        alert("Error connecting to elasticsearch!!")
+      }
+    });
+    })(counter);
+    }
 		},
-		userHelpPage : function(){
+		counter : function(){
 			window.open("https://github.com/prasadtalasila/packetanalyzer",'_blank');
 		},
 		userLogout  : function(){
--- src/main/webapp/WEB-INF/js/views/config-playground-view.js
+++ src/main/webapp/WEB-INF/js/views/config-playground-view.js
@@ -9,8 +9,12 @@ window.ConfigPlaygroundView = Backbone.View.extend({
 	},
 	initialize: function () {
 		this.delegateEvents();
+    this.userParseGraph = null;
+    this._layers = [];
 	},	
-
+  disableAnalyzeButton:function(){
+    document.getElementById('analyzeBtn').disabled = true; 
+  },
 	userHelpPage : function(){
 	  window.open("https://github.com/prasadtalasila/packetanalyzer",'_blank');
 	},
@@ -20,21 +24,24 @@ window.ConfigPlaygroundView = Backbone.View.extend({
 		Cookies.remove('userAuth');		
     
 		app.navigate("#",{trigger: true});
-		alert("You have been logged out. Please login to co ntinue");
+		alert("You have been logged out. Please login to continue");
         return false;
 	},
 	analysis : function(event){
 		event.preventDefault();
+    _this = this;
     var pcapPath = sessionStorage.getItem('pcapPath');
     $.ajax({
       url:'/protocolanalyzer/session/analysis',
       type:'GET',
       contentType: 'application/json; charset=utf-8',
       dataType:'text',
-      data: { pcapPath:pcapPath },
+      data: {pcapPath : pcapPath,graph : _this.userParseGraph},
       success:function (data) {
           var jsonData = JSON.parse(data);
-          var sessionNumber = jsonData.sessionNumber;
+          var sessionName = jsonData.sessionName;
+          sessionStorage.setItem('sessionName',sessionName);
+          sessionStorage.setItem('layers',_this._layers);
           app.navigate("#/analysis",{trigger: true});
         },
         error:function(){
@@ -42,92 +49,140 @@ window.ConfigPlaygroundView = Backbone.View.extend({
         }
       });
 	},
-  readSingleFile : function(evt) {
-    //Retrieve the first (and only!) File from the FileList object
-    var f = document.getElementById("fileInput").files[0]; 
-
-    if (f) {
-      
-      var r = new FileReader();
-      r.onload = function(e) { 
-          var userParseGraph = e.target.result;
-          $.ajax({
-            url:'/protocolanalyzer/session/analysis',
-             type:'GET',
-             contentType: 'application/json; charset=utf-8',
-             dataType:'text',
-             data: {graph : userParseGraph},
-             success:function (data) {
-              
-            	 var jsonData = JSON.parse(data);
-            	 var status = jsonData.status;
-                 if(status==="success"){
-                    app.navigate("#/analysis");
-                 }
-                 else{
-                    alert("Error!!!");  
-                 }
-             },
-             error:function(){
-                alert("Something went wrong. Please try again later.");
-             }
-             });
-      }
-      r.readAsText(f);
-     
-    } else { 
-      alert("Failed to load file");
-    }
-  },
 
   graphValidation : function(){
     var f = document.getElementById("fileInput").files[0]; 
+    _this = this;
     if (f) {
       var r = new FileReader();
       r.onload = function(e) { 
           var flag =0;
           //test graph is the main graph, user graph checked against this
-          var testGraph = 'graph _start_ { ethernet;} graph _ethernet_ { switch(ethertype) {case 0x800: ipv4;}} graph _ipv4_ {switch(protocol) {case 0x06: tcp;}}graph _tcp_ {}graph _end_ {}';
+          //ECMA 6 : replace with backticks , is cleaner
+          var testGraph = 'graph start {'+
+            'ethernet;'+
+          '}'+
+          'graph ethernet {'+
+            'switch(ethertype) {'+
+              'case 0800: ipv4;'+
+            '}'+
+          '}'+
+          'graph ipv4 {'+
+            'switch(protocol) {'+
+              'case 06:  tcp;'+
+            '}'+
+          '}'+
+          'graph tcp {'+
+          '}'+
+          'graph end {'+
+          '}';
+          var flag = 0;
           var testParsing = testGraph.split(/[\{\}]/);
           for (var i = 0; i < testParsing.length; i++) {
             testParsing[i] = testParsing[i].trim();
           }
           //user provided p4 graph
-          var userParseGraph = e.target.result;
-          var userParsing = userParseGraph.split(/[\{\}]/);
+          var graphIndices =[]; //for keeping index of the graph elements
+          var graphValues= []; //for keeping value of graph elements
+          var indexOfGraphElements=0;
+          var mainCounter =0; //for seeing if graph has appropriate number of layer matches
+
+          _this.userParseGraph = e.target.result;
+          var userParsing = _this.userParseGraph.split(/[\{\};]/);
           for (var i = 0; i < userParsing.length; i++) {
             userParsing[i] = userParsing[i].trim();
-          }
-          if(userParsing.length > testParsing.length ){
-            alert("Invalid P4 graph");
-            flag++;
-          }
-          // initial node check
-          for(var i=0;i< 2;i++){
-            if(userParsing[i]!==testParsing[i]){
-              alert("Invalid P4 graph");
-              flag++;
-            }
-          }
-          //other nodes
-          for(var i=2;i<userParsing.length-4;i++){
-            if(userParsing[i]!==testParsing[i]){
-              alert("Invalid P4 graph");
-              flag++;
+            if(userParsing[i].search('graph')===0){
+              graphIndices[indexOfGraphElements] = i;
+              graphValues[indexOfGraphElements] = userParsing[i];
+              indexOfGraphElements++;
+            }  
+            if(userParsing[i].search('switch')===0){    
+              mainCounter++;
             }  
           }
-          if(flag===0)
-            alert("Valid configuration");
+          for (var k =0;k<indexOfGraphElements;k++){
+            _this._layers[k] =  graphValues[k].split(' ')[1];
+            if(k===0 && _this._layers[k] !=='start') {
+              //start condition check
+              flag++;
+            }
+            if(k===(indexOfGraphElements-1) && _this._layers[k]!=='end' ) { 
+              //end condition check
+              flag++; 
+            }
+          }  
+          //trim results
+          for (var i = 0; i < _this._layers.length; i++) {
+            _this._layers[i] = _this._layers[i].trim();
+          }
+          //start node check
+          if(userParsing[1] !== _this._layers[1]){
+            flag++;
+          }
+          //create map of next _this._layers : key is current layer and values are all the possibilities for next layer
+          var nextLayers={};
+          var countCases =0;
+          var countCases2=0;
+          for(var i =0;i< userParsing.length;i++){
+            if(userParsing[i].search('graph')===0){
+              nextLayers[_this._layers[countCases-1]] = nextLayerList;
+              var nextLayerList=[];
+              countCases++;
+              countCases2=0;
+            }
+            if(userParsing[i].search('case')===0){ 
+              var temp = userParsing[i].split(' ')[2].trim();
+              nextLayerList.push(temp);
+              countCases2++;
+            }
+          }
+          // _this._layers contains the list of _this._layers possible for this experiment.
+          //nextLayers is a 2D array containing the next possible _this._layers for each layer.
+          //checking valid next _this._layers
+          var matchOneLayerToNext =0;
+          for(var i=0;i < _this._layers.length-1;i++){ 
+          //length-1 because the last layer will not have cases within it, even if it does, those are ignored
+            for(key in nextLayers){
+              if(key===_this._layers[i]){
+                var temp = i+1;
+                if(_this.isInArray(nextLayers[key],_this._layers[temp])){
+                  matchOneLayerToNext++;
+                }  
+              }
+            }  
+          }
+          if(matchOneLayerToNext!==mainCounter){
+            flag++;
+          }
+          if(flag===0){
+            alert("P4 Graph is valid");
+            document.getElementById('analyzeBtn').disabled = false; 
+          }
+          else{
+            alert("P4 Graph is not valid, please enter a valid configuration");
+          }
       }
       r.readAsText(f);
-     
     } else { 
       alert("Failed to load file");
     }
   },
-
+  isInArray: function(array, search)
+  {
+    return array.indexOf(search) >= 0;
+  },
+  indices : function(source,find){
+    var result = [];
+    for (i = 0; i < source.length; ++i) {
+      if (source.substring(i, i + find.length) == find) {
+        result.push(i);
+      }
+    }
+    return result;
+  },
 	render: function () {
 		$(this.el).html(this.template());
+    $(document).ready(this.disableAnalyzeButton);
 		return this;
 	}
 });
